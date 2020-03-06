facf9894cbec6d2144d6d8ecfe62456ebcf53f1c
allccodes <almeida.pcordeiro@gmail.com>
2020-03-06 17:41:23 +0000

    database engine

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nrpywmwo:13GgkLyZGfbz_dmFSMjDwm8vx3N2okSE@rogue.db.elephantsql.com:5432/nrpywmwo'
 db = SQLAlchemy(app)
 
 
--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 DEBUG = True
 SECRET_KEY = "This is a secret"
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/expenses01'
+SQLALCHEMY_DATABASE_URI = 'postgres://nrpywmwo:13GgkLyZGfbz_dmFSMjDwm8vx3N2okSE@rogue.db.elephantsql.com:5432/nrpywmwo'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
