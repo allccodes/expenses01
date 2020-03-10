b70e14dbdf7e02d0c8f950b71fe42070f2c49995
allccodes <almeida.pcordeiro@gmail.com>
2020-03-10 17:44:49 +0000

    file

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from app import app, db, bcrypt
 from flask import render_template, flash, redirect, url_for, request
 from results import Expenses
-from forms import RegistrationForm, LoginForm
+from forms import RegistrationForm, LoginForm, UpdateAccountForm
 from app.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -80,11 +80,21 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route("/account")
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account as been updated', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', image_file=image_file)
+    return render_template('account.html', image_file=image_file, form=form)
 
 
 
